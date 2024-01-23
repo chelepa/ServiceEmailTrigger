6d06df91c58ab62749ee7a9470582e09d890b97b
Claudimir Chelepa <cl.chelepa@gmail.com>
2024-01-22 22:53:07 -0300

    init commit

---
+++ src/main/java/br/com/ServiceEmailTrigger/ServiceEmailTriggerApplication.java
@@ -0,0 +1,13 @@
+package br.com.ServiceEmailTrigger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceEmailTriggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceEmailTriggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ServiceEmailTrigger/ServiceEmailTriggerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ServiceEmailTrigger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceEmailTriggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
